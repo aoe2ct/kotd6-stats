8c4e841ffb28c0fa6da1bb9c2e64a2d56d0d7bd7
Stéphane Bisinger <stephane@sbisinger.ch>
2025-12-21 04:51:43 +0100

    Reorganize in multiple tables

--- src/components/finalist.tsx
+++ src/components/finalist.tsx
@@ -47,58 +47,34 @@ export default function Finalist({ name }) {
             <td>Favourite Colour</td>
             <td>{playerData['color']}</td>
           </tr>
+        </tbody>
+      </table>
+      <table className={styles.civs}>
+        <colgroup>
+          <col width={400} />
+          <col />
+        </colgroup>
+        <thead>
           <tr>
-            <td colSpan={2}>Time played</td>
+            <th colSpan={2}>Time played</th>
           </tr>
-          <tr className={styles.subrow}>
+        </thead>
+        <tbody>
+          <tr>
             <td>Overall</td>
             <td>{playerData?.duration?.cumulative}</td>
           </tr>
-          <tr className={styles.subrow}>
+          <tr>
             <td>Longest game</td>
             <td>{playerData?.duration?.longest}</td>
           </tr>
-          <tr className={styles.subrow}>
+          <tr>
             <td>Shortest game</td>
             <td>{playerData?.duration?.shortest}</td>
           </tr>
-          <tr className={styles.subrow}>
+          <tr>
             <td>Median</td>
             <td>{playerData?.duration?.median}</td>
-          </tr>
-          <tr>
-            <td colSpan={2}>eAPM</td>
-          </tr>
-          <tr className={styles.subrow}>
-            <td>Max</td>
-            <td>{playerData?.eapm?.max}</td>
-          </tr>
-          <tr className={styles.subrow}>
-            <td>Min</td>
-            <td>{playerData?.eapm?.min}</td>
-          </tr>
-          <tr className={styles.subrow}>
-            <td>Median</td>
-            <td>{playerData?.eapm?.median}</td>
-          </tr>
-          <tr>
-            <td colSpan={2}>Vils queued</td>
-          </tr>
-          <tr className={styles.subrow}>
-            <td>Max</td>
-            <td>{playerData?.vils_queued?.max}</td>
-          </tr>
-          <tr className={styles.subrow}>
-            <td>Min</td>
-            <td>{playerData?.vils_queued?.min}</td>
-          </tr>
-          <tr className={styles.subrow}>
-            <td>Median</td>
-            <td>{playerData?.vils_queued?.median}</td>
-          </tr>
-          <tr className={styles.subrow}>
-            <td>Overall</td>
-            <td>{playerData?.vils_queued?.total}</td>
           </tr>
         </tbody>
       </table>
@@ -160,6 +136,60 @@ export default function Finalist({ name }) {
                 <span>{playerData?.civ_played_longest?.name} ({playerData?.civ_played_longest?.duration})</span>
               </div>
             </td>
+          </tr>
+        </tbody>
+      </table>
+      <table className={styles.civs}>
+        <colgroup>
+          <col width={400} />
+          <col />
+        </colgroup>
+        <thead>
+          <tr>
+            <th colSpan={2}>eAPM</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Max</td>
+            <td>{playerData?.eapm?.max}</td>
+          </tr>
+          <tr>
+            <td>Min</td>
+            <td>{playerData?.eapm?.min}</td>
+          </tr>
+          <tr>
+            <td>Median</td>
+            <td>{playerData?.eapm?.median}</td>
+          </tr>
+        </tbody>
+      </table>
+      <table className={styles.civs}>
+        <colgroup>
+          <col width={400} />
+          <col />
+        </colgroup>
+        <thead>
+          <tr>
+            <th colSpan={2}>Vils queued</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Max</td>
+            <td>{playerData?.vils_queued?.max}</td>
+          </tr>
+          <tr>
+            <td>Min</td>
+            <td>{playerData?.vils_queued?.min}</td>
+          </tr>
+          <tr>
+            <td>Median</td>
+            <td>{playerData?.vils_queued?.median}</td>
+          </tr>
+          <tr>
+            <td>Overall</td>
+            <td>{playerData?.vils_queued?.total}</td>
           </tr>
         </tbody>
       </table>
